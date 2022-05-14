b916385137e20ee26062967826f29c06caa8871b
bischraf <bischraf@students.zhaw.ch>
2022-05-14 11:03:02 +0200

    finished lab 12

---
+++ 12_lab_FSM_Lift/project/app/state_machine.c
@@ -0,0 +1,239 @@
+/* ----------------------------------------------------------------------------
+ * --  _____       ______  _____                                              -
+ * -- |_   _|     |  ____|/ ____|                                             -
+ * --   | |  _ __ | |__  | (___    Institute of Embedded Systems              -
+ * --   | | | '_ \|  __|  \___ \   Zurich University of                       -
+ * --  _| |_| | | | |____ ____) |  Applied Sciences                           -
+ * -- |_____|_| |_|______|_____/   8401 Winterthur, Switzerland               -
+ * ----------------------------------------------------------------------------
+ * --
+ * -- Description:  Implementation of module state_machine.
+ * --
+ * -- $Id: state_machine.c 5526 2022-01-18 07:26:31Z ruan $
+ * ------------------------------------------------------------------------- */
+
+/* standard includes */
+#include <stdint.h>
+
+/* user includes */
+#include "state_machine.h"
+#include "action_handler.h"
+#include "timer.h"
+
+
+/* -- Macros
+ * ------------------------------------------------------------------------- */
+
+#define SAFETY_DURATION      150u       // 150 * 10ms = 1.5s
+#define SIGNAL_DURATION      100u       // 100 * 10ms = 1s
+
+#define TEXT_F0_OPENED       "F0_OPENED"
+#define TEXT_F0_CLOSED       "F0_CLOSED"
+#define TEXT_F1_OPENED       "F1_OPENED"
+#define TEXT_F1_CLOSED       "F1_CLOSED"
+#define TEXT_MOVING_UP       "MOVING_UP"
+#define TEXT_MOVING_DOWN     "MOVING_DOWN"
+
+/// STUDENTS: To be programmed
+#define TEXT_WAIT_MOVING_UP     "WAIT_MOVING_UP"
+#define TEXT_WAIT_MOVING_DOWN     "WAIT_MOVING_DOWN"
+/// END: To be programmed
+
+
+/* -- Type definitions
+ * ------------------------------------------------------------------------- */
+
+// definition of FSM states
+typedef enum {
+    /* task 4.1 */
+    F0_OPENED,
+    F0_CLOSED,
+    
+    /* task 4.2 */
+    F1_OPENED,
+    F1_CLOSED,
+    MOVING_UP,
+    MOVING_DOWN,
+
+    /// STUDENTS: To be programmed
+		WAIT_MOVING_UP,
+		WAIT_MOVING_DOWN,
+    /// END: To be programmed
+   
+} state_t;
+
+
+/* Module-wide variables & constants
+ * ------------------------------------------------------------------------- */
+
+// current FSM state 
+static state_t state = F0_CLOSED;
+
+
+/* Public function definitions
+ * ------------------------------------------------------------------------- */
+
+/*
+ * See header file
+ */
+void fsm_init(void)
+{
+    action_handler_init();
+    ah_show_exception(NORMAL, "");
+    
+    /* go to initial state & do initial actions */
+    
+    /// STUDENTS: To be programmed
+		ah_show_state(TEXT_F0_CLOSED);
+    /// END: To be programmed
+}
+
+
+/*
+ * See header file
+ */
+void fsm_handle_event(event_t event)
+{
+    /// STUDENTS: To be programmed
+		switch(state){	
+			//STATE F0_CLOSED
+			case F0_CLOSED:
+					//SWITCH EVENT
+					switch(event){
+						case EV_DOOR0_OPEN_REQ:
+								ah_door(DOOR_OPEN);
+								ah_show_state(TEXT_F0_OPENED);
+								state = F0_OPENED;
+							break;
+						case EV_BUTTON_F1:
+								ah_door(DOOR_LOCK);
+								timer_start(SAFETY_DURATION);
+								ah_show_state(TEXT_WAIT_MOVING_UP);
+								state = WAIT_MOVING_UP;
+							break;
+						default:
+								state = F0_CLOSED;
+							break;					
+					}
+				break;
+
+			//STATE F0_OPENED		
+			case F0_OPENED:				
+					//SWITCH EVENT
+					switch(event){
+						case EV_DOOR0_CLOSE_REQ:
+								ah_door(DOOR_CLOSE);
+								ah_show_state(TEXT_F0_CLOSED);
+								state = F0_CLOSED;
+							break;
+						default:
+								state = F0_OPENED;
+							break;					
+					}
+				break;
+					
+			//STATE MOVING_UP	
+			case MOVING_UP:
+					//SWITCH EVENT
+					switch(event){
+						case EV_F1_REACHED:
+								ah_motor(MOTOR_OFF);
+								ah_door(DOOR_UNLOCK);
+								ah_show_state(TEXT_F1_CLOSED);
+								state = F1_CLOSED;
+							break;
+						default:
+								state = MOVING_UP;
+							break;
+					}
+				break;
+			
+			//STATE F1_CLOSED
+			case F1_CLOSED:
+					//SWITCH EVENT
+					switch(event){
+						case EV_DOOR1_OPEN_REQ:
+								ah_door(DOOR_OPEN);
+								ah_show_state(TEXT_F1_OPENED);
+								state = F1_OPENED;
+							break;
+						case EV_BUTTON_F0:
+								ah_door(DOOR_LOCK);
+								timer_start(SAFETY_DURATION);
+								ah_show_state(TEXT_WAIT_MOVING_DOWN);
+								state = WAIT_MOVING_DOWN;
+							break;						
+						default:
+								state = F1_CLOSED;
+							break;
+					}
+				break;
+					
+				//STATE F1_OPENED
+				case F1_OPENED:
+						//SWITCH EVENT
+						switch(event){
+							case EV_DOOR1_CLOSE_REQ:
+									ah_door(DOOR_CLOSE);
+									ah_show_state(TEXT_F1_CLOSED);
+									state = F1_CLOSED;
+								break;
+							default:
+									state = F1_OPENED;
+								break;							
+						}
+					break;
+
+				//STATE MOVING_DOWN
+				case MOVING_DOWN:
+						//SWITCH EVENT
+						switch(event){
+							case EV_F0_REACHED:
+									ah_motor(MOTOR_OFF);
+									ah_door(DOOR_UNLOCK);
+									ah_show_state(TEXT_F0_CLOSED);
+									state = F0_CLOSED;
+								break;
+							default:
+									state = MOVING_DOWN;
+								break;
+						}
+					break;
+						
+				//STATE WAIT_MOVING_UP
+				case WAIT_MOVING_UP:
+						//SWITCH EVENT
+						switch(event){
+							case EV_TIMEOUT:
+									ah_motor(MOTOR_UP);
+									ah_show_state(TEXT_MOVING_UP);
+									state = MOVING_UP;
+								break;
+							default:
+									state = WAIT_MOVING_UP;
+								break;
+						}
+					break;
+						
+				//STATE WAIT_MOVING_DOWN
+				case WAIT_MOVING_DOWN:
+						
+						//SWITCH EVENT
+						switch(event){
+							case EV_TIMEOUT:
+									ah_motor(MOTOR_DOWN);
+									ah_show_state(TEXT_MOVING_DOWN);
+									state = MOVING_DOWN;
+								break;
+							default:
+									state = WAIT_MOVING_DOWN;
+								break;
+						}
+					break;
+			
+			//STATE default
+			default:	
+				break;
+		}
+    /// END: To be programmed
+}
---
+++ 12_lab_FSM_Lift/project/build/state_machine.d
@@ -0,0 +1,7 @@
+.\build\state_machine.o: app\state_machine.c
+.\build\state_machine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\build\state_machine.o: app\state_machine.h
+.\build\state_machine.o: app\event_handler.h
+.\build\state_machine.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\build\state_machine.o: app\action_handler.h
+.\build\state_machine.o: app\timer.h
